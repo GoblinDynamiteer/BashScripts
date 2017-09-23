78d466800689134ff7c39a023931b77af3d74604
Johan Kämpe <kampe.johan@gmail.com>
2017-09-23 23:19:06 +0200

    Added comments

--- Tests/ping_check.sh
+++ Tests/ping_check.sh
@@ -1,5 +1,9 @@
+# $? is the exit status of the last command
+# executed, in this case ping
+
 HOST="google.com"
 ping -c 1 $HOST
+
 if [ "$?" -eq "0" ] # if..fi block start
 then
     echo "$HOST reachable"
