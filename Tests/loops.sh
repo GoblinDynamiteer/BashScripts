cb3525dd9633b58248ad6deac964bfa8e4282cf8
Johan Kämpe <kampe.johan@gmail.com>
2017-09-23 22:35:13 +0200

    Loops

---
+++ Tests/loops.sh
@@ -0,0 +1,29 @@
+#while-loop
+
+i=1 # i = 1 is invalid, dont use spaces
+while [ $i -le 3 ] # -le less than or equal to
+do
+    echo $i
+    ((i++)) # double parentheses
+done
+
+echo "While loop done!"
+
+#until loop
+j=1
+until [ $j -gt 3 ]
+do
+    echo $j
+    ((j++))
+done
+
+echo "Until loop done!"
+
+#for loop
+n='1 2 3'
+for no in $n
+do
+    echo $no
+done
+
+echo "For loop done"
