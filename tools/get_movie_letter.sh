e7900a0a7ebb59558fd498fe78a3e634fe5407d4
Johan Kämpe <kampe.johan@gmail.com>
2018-01-26 23:14:18 +0100

    Get movie letter from directory name

---
+++ tools/get_movie_letter.sh
@@ -0,0 +1,20 @@
+# Determine movie folder letter
+# ^^ = to uppercase
+# ,, = to lowercase
+
+DIR=$( basename "$1" ) # get base of path
+echo $DIR
+THE="${DIR:0:4}"
+
+if [ "${THE,,}" == "the." ]; then
+    FIRST="${DIR:4:1}"
+else
+    FIRST="${DIR:0:1}"
+fi
+
+if [ "${FIRST,,}" == "v" ] || [ "${FIRST,,}" == "w" ]; then
+    FIRST="VW"
+fi
+
+echo ${FIRST^^}
+
